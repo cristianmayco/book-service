bee2a81e77d3914294ac276efcac2a7ad5176248
Cristian Mayco da Costa <cristianmayco@gmail.com>
2021-11-20 16:56:45 -0300

    Add book service and dependencies(JPA, docker, flyway)

---
+++ src/main/resources/db/migration/V1__Create_Table_Books.sql
@@ -0,0 +1,10 @@
+﻿CREATE SEQUENCE book_seq;
+
+CREATE TABLE book
+(
+    id          INT DEFAULT NEXTVAL('book_seq') PRIMARY KEY,
+    author      text,
+    lauch_date timestamp(6)   NOT NULL,
+    price       decimal(65, 2) NOT NULL,
+    title       text
+);
