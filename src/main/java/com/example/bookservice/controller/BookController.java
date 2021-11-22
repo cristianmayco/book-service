27d91d4611a0a60e28e470fd3df72707ead719e3
Cristian Mayco da Costa <cristianmayco@gmail.com>
2021-11-22 09:48:16 -0300

    show services port

--- src/main/java/com/example/bookservice/controller/BookController.java
+++ src/main/java/com/example/bookservice/controller/BookController.java
@@ -34,7 +34,7 @@ public class BookController {
         var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
 
         var port = env.getProperty("local.server.port");
-        book.setEnvironment(port);
+        book.setEnvironment("Book port:" +  port + "cambio port: " +  cambio.getEnvironment());
         book.setPrice(cambio.getConvertedValue());
 
         return book;
