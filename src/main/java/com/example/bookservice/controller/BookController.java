fb4f449967ea4d0b85fdfd9c25c430a2921c9388
Cristian Mayco da Costa <cristianmayco@gmail.com>
2021-11-22 08:11:03 -0300

    Add feign support and remove rest template

--- src/main/java/com/example/bookservice/controller/BookController.java
+++ src/main/java/com/example/bookservice/controller/BookController.java
@@ -1,17 +1,14 @@
 package com.example.bookservice.controller;
 
 import com.example.bookservice.model.Book;
+import com.example.bookservice.proxy.CambioProxy;
 import com.example.bookservice.repository.BookRepository;
-import com.example.bookservice.response.Cambio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
-
-import java.util.HashMap;
 
 @RestController
 @RequestMapping("book-service")
@@ -23,6 +20,8 @@ public class BookController {
     @Autowired
     private BookRepository repository;
 
+    @Autowired
+    private CambioProxy proxy;
 
     @GetMapping(value = "/{id}/{currency}")
     public Book findBook(
@@ -32,20 +31,7 @@ public class BookController {
         var book = repository.getById(id);
         if (book == null) throw new RuntimeException("Book not found");
 
-        HashMap<String, String> params = new HashMap<>();
-        params.put("amount",book.getPrice().toString() );
-        params.put("from", "USD");
-        params.put("to", currency);
-
-        var response = new RestTemplate()
-                .getForEntity("http://localhost:8000/cambio-service/" +
-                                "{amount}/{from}/{to}",
-                        Cambio.class,
-                        params);
-
-        var cambio = response.getBody();
-
-
+        var cambio = proxy.getCambio(book.getPrice(), "USD", currency);
 
         var port = env.getProperty("local.server.port");
         book.setEnvironment(port);
